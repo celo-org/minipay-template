1dcece777f307df36ce507d4bd585ca882ed0354
Viral Sangani <viral.sangani2011@gmail.com>
2024-07-12 14:33:06 +0200

    chore: Update dependencies and add RainbowKitProvider and WagmiProvider to _app.tsx

--- packages/react-app/components/Header.tsx
+++ packages/react-app/components/Header.tsx
@@ -1,7 +1,22 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useConnect } from "wagmi";
+import { injected } from "wagmi/connectors";
 
 export default function Header() {
+  const [hideConnectBtn, setHideConnectBtn] = useState(false);
+  const { connect } = useConnect();
+
+  useEffect(() => {
+    if (window.ethereum && window.ethereum.isMiniPay) {
+      setHideConnectBtn(true);
+      connect({ connector: injected({ target: "metaMask" }) });
+    }
+  }, []);
+
   return (
     <Disclosure as="nav" className="bg-colors-primary border-b border-black">
       {({ open }) => (
@@ -20,14 +35,33 @@ export default function Header() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center text-white font-semibold text-2xl">
-                  <p>Minipay</p>
+                <div className="flex flex-shrink-0 items-center">
+                  <Image
+                    className="block h-8 w-auto sm:block lg:block"
+                    src="/logo.svg"
+                    width="24"
+                    height="24"
+                    alt="Celo Logo"
+                  />
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  <a className="inline-flex items-center border-b-2 border-black px-1 pt-1 text-sm font-medium text-gray-900">
+                  <a
+                    href="#"
+                    className="inline-flex items-center border-b-2 border-black px-1 pt-1 text-sm font-medium text-gray-900"
+                  >
                     Home
                   </a>
                 </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {!hideConnectBtn && (
+                  <ConnectButton
+                    showBalance={{
+                      smallScreen: true,
+                      largeScreen: false,
+                    }}
+                  />
+                )}
               </div>
             </div>
           </div>
@@ -48,10 +82,4 @@ export default function Header() {
       )}
     </Disclosure>
   );
-}
-
-declare global {
-  interface Window {
-    ethereum: any;
-  }
 }
