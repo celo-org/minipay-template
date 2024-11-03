46140edcfc7039fdd653d13e96b0a1f4069911ec
GigaHierz <lenahierzi@hotmail.de>
2024-11-04 00:57:35 +0700

    add dos for components and Vercel

--- packages/react-app/next.config.js
+++ packages/react-app/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["pbs.twimg.com", "cdn-production-opera-website.operacdn.com"],
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
